9c5b2d7bd4217710d5bd7294917acb817be4fd42
碎月 <suiyue@suiyuedemacbook-air.local>
2016-01-26 23:14:57 +0800

    初始化项目

---
+++ Contact/Contact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contact
+//
+//  Created by 碎月 on 16/1/26.
+//  Copyright © 2016年 碎月. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contact/Contact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contact
+//
+//  Created by 碎月 on 16/1/26.
+//  Copyright © 2016年 碎月. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
